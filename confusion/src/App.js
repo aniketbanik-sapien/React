569433542dbb7697ddbd5125d23717aa31eeeb21
aniketbanik-sapien <aniket.banik012@nmims.edu.in>
2022-04-24 21:23:00 +0500

    day 2

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,18 +1,17 @@
-// import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
+
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  render(){
   return (
-    <div className="App">
-      <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-      
+    <div>
+
+      <Main />
     </div>
   );
 }
-
+}
 export default App;
